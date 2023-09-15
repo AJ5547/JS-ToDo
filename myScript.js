affa8422a27cf269572c944a369c325bfd938749
AJ5547 <alissa.jackson5547@stu.mtec.edu>
2023-09-15 09:56:26 -0600

    All my files

--- myScript.js
+++ myScript.js
@@ -1,5 +1,11 @@
+function addTask(){
+    const newTask = document.getElementById("newTask");
+    newTask.innerHTML = '<input id="task" /> <button onclick="add()"> Add </button> ';
 
-function listItem(){
-   const list = document.getElementById("list");
-   list.innerHTML += '<div class="flex"> <input type="checkbox" /> <h3> List Name </h3><br> <p> What it is here </p> </div>'
+}
+function add() {
+  const info = document.getElementById("task").value;
+  const taskItems = document.getElementById("list");
+  taskItems.innerHTML +=`<div class="flex"> <input type="checkbox" /> <p> ${info} </p> </div>`;
+  document.getElementById("task").value = "";
 }
