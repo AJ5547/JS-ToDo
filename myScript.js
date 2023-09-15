d04e216920461251564cd21e89edb4ea488f5a49
AJ5547 <alissa.jackson5547@stu.mtec.edu>
2023-09-15 08:15:56 -0600

    Inital

---
+++ myScript.js
@@ -0,0 +1,5 @@
+
+function listItem(){
+   const list = document.getElementById("list");
+   list.innerHTML += '<div class="flex"> <input type="checkbox" /> <h3> List Name </h3><br> <p> What it is here </p> </div>'
+}
