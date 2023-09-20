d02211b4d4d4ebe7e0f784945ce76ab1ff6cc587
AJ5547 <alissa.jackson5547@stu.mtec.edu>
2023-09-20 07:51:00 -0600

    Changing how the list names work

--- myScript.js
+++ myScript.js
@@ -1,11 +1,18 @@
+"use strict";
+
+//Brings up a small form
 function addTask(){
     const newTask = document.getElementById("newTask");
-    newTask.innerHTML = '<input type="text" id="task" placeholder="list name" required /> <button onclick="addList()"> Add </button> ';
+    newTask.innerHTML = '<input type="text" id="task" placeholder="list name" required /> <button class="text-white" onclick="list()"> Add </button> ';
 }
-function addList() {
+//Uses the button above to create form name
+function list() {
   const info = document.getElementById("task").value;
   const taskItems = document.getElementById("todoLists");
-  taskItems.innerHTML +=`<h3 class="bg-gray-500 p-1"> ${info} </h3>`;
+  taskItems.innerHTML +=`<div class="flex items-center justify-between bg-gray-400 w-full">
+  <h3 class="p-1"> ${info} </h3>
+  <i class="fa-solid fa-trash-can mr-2" style="color: #fe0717;"></i>
+</div>`;
   document.getElementById("task").value = "";
   document.getElementById("newTask").innerHTML = "";
 }
